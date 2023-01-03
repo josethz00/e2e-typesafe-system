624b18e1421a880114aaca36b3939767856bcaa3
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-01-03 16:34:48 -0300

    chore: deployment ready

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,20 +1,18 @@
+import { useQuery } from 'urql'
+import { GetUsersDocument } from './graphql/generated'
+
 import UserDisplay from './components/UserDisplay'
 import { User } from './types'
 
 function App() {
-  const users: User[] = [{
-    name: 'Prisma Fan',
-    messages: [{
-      body: 'Prisma rocks!!'
-    }, {
-      body: 'Did I mention I love Prisma?'
-    }]
-  }]
+  const [results] = useQuery({
+    query: GetUsersDocument
+  })
 
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
       {
-        users.map((user, i) => <UserDisplay user={user} key={i}/>)
+        results.data?.users.map((user, i) => <UserDisplay user={user} key={i}/>)
       }
     </div>
   )
